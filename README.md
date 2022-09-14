215cd97ae10e64ed916de7a5efe6b3ee52a58e5d
Graham Hukill <ghukill@gmail.com>
2022-09-14 07:42:21 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # qb_philosophy_engine
+
+## Installation
+
+```shell
+python3.10 -m venv venv
+source venv/bin/activate
+```
+
+## Resources
+  * rulebook: https://drive.google.com/file/d/1gKQZEZD67oYQltUWoSXlxxoVddxOQJvd/view
+
+## Inspiration and Scratches
+  * Go (pygame): https://towardsdatascience.com/python-miniproject-making-the-game-of-go-from-scratch-in-pygame-d94f406d4944
---
+++ core/tiles.py
@@ -0,0 +1,29 @@
+"""
+
+Tile Types:
+    - Push
+        - moves target 1 away
+    - Corner Push
+        - moves target 1 diagonal away
+    - Slide Left
+        - moves target 1 left
+    - Slide Right
+        - moves target 1 right
+    - Pull Left
+        - moves target left and towards (1 diagonal)
+    - Pull Right
+        - moves target right and towards (1 diagonal)
+    - Long Shot
+        - targets 2 spaces away, moves target 1 away
+    - Corner Long Shot
+        - targets 2 spaces away, moves target 1 diagonal away
+    - Decision
+        - moves target 1 left OR right [decision]
+    - Rephrase
+        - rotate target X degrees
+        - can target your own tiles
+    - Toss
+        - moves target one space behind self
+    - Persuade
+        - moves target and self 1 backwards
+"""
